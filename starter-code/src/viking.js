1467c723a039be9fb6fe560d761d50d6269c4c93
allanac <acole006@fiu.edu>
2017-10-23 22:10:40 -0400

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,55 @@
 // Soldier
-function Soldier () {}
+function Soldier (strength, health) {
+  this.strength = strength;
+  this.health = health;
+}
+
+Soldier.prototype.attack = function () {
+  return this.strength;
+}
+Soldier.prototype.receiveDamage = function(damage){
+   this.health = this.health - damage;
+}
 
 // Viking
-function Viking () {}
+function Viking (name, health, strength) {
+  Soldier.call (this, name, health, strength);
+}
+Viking.prototype = Object.create(Soldier.protoype);
+
+Viking.protoype.attack = function () {
+  return this.strength;
+}
+
+Viking.prototype.receiveDamage = function(damage){
+
+  this.health = this.health - damage;
+  return this.health;
+
+  if (this.health <= 0) {
+    console.log (this.name + "has died in the act of combat")
+  }
+  else {
+  console.log (this.name + "has received" + damage + "points of damage")}
+};
+
+
 
 // Saxon
-function Saxon () {}
+function Saxon (health, strength) {
+  Soldier.call (this, health, strength);
+}
+Soldier.prototype = Object.create(Soldier.prototype);
+Soldier.prototype.attack = function (){
+  return this.strength;
+}
+
+Soldier.prototype.receiveDamage = function (damage){
+
+
+  console.log ("A" + this.name + "has received " + damage + "points of damage")
+  console.log("A" + this.name + "has died in combat" )
+};
 
 // War
 function War () {}
